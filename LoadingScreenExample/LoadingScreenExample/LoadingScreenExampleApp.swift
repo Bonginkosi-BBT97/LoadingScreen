62b5113ef87d53cb4eb36dda03fb27c1b5b25da8
Nkosi.T <Nkositshabalala@glucode.com>
2023-01-13 10:54:51 +0200

    first commit

---
+++ LoadingScreenExample/LoadingScreenExample/LoadingScreenExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadingScreenExampleApp.swift
+//  LoadingScreenExample
+//
+//  Created by Bonginkosi Tshabalala on 2023/01/13.
+//
+
+import SwiftUI
+
+@main
+struct LoadingScreenExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
